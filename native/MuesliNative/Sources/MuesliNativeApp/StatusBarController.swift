609fa740ac8f4f660458a04efab389d18257a7ff
Shaheen <mxshaheen@gmail.com>
2026-08-19 02:35:57 +0300

    refactor: retire fixed indicator settings
    Signed-off-by: Shaheen <mxshaheen@gmail.com>

--- native/MuesliNative/Sources/MuesliNativeApp/StatusBarController.swift
+++ native/MuesliNative/Sources/MuesliNativeApp/StatusBarController.swift
@@ -123,10 +123,6 @@ final class StatusBarController: NSObject, NSMenuDelegate {
         } else {
             menu.addItem(actionItem(title: "Start Meeting Recording", action: #selector(MuesliController.toggleMeetingRecording)))
         }
-        let indicatorTitle = controller.config.showFloatingIndicator
-            ? "Hide Floating Button"
-            : "Show Floating Button"
-        menu.addItem(actionItem(title: indicatorTitle, action: #selector(MuesliController.toggleFloatingIndicatorVisibility)))
         menu.addItem(.separator())
 
         let recentItem = NSMenuItem(title: "Recent Dictations", action: nil, keyEquivalent: "")
